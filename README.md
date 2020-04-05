78e9dd071a9e3eea65cb87bd498a74ee44787be8
Break Yang <breakds@gmail.com>
2020-04-04 17:33:46 -0700

    Add osmium as dependencies in shell.nix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # open-semantic-map
 A (currently) experimental repo for Open Street Map based algorithm library. 
+
+## Interact with Open Street Map Files (PBF)
+
+OSM `pbf` files are nothing but protocol buffer records. The tool
+[osmium](https://osmcode.org/file-formats-manual/) can be used to
+interact with them.
---
+++ shell.nix
@@ -0,0 +1,19 @@
+let pkgs = import <nixpkgs> {
+      config.allowUnfree = true;
+    };
+
+    osm-python = pkgs.python3.withPackages (python-packages: with python-packages; [
+      pyyaml
+    ]);
+
+in pkgs.mkShell rec {
+  name = "osm";
+  buildInputs = with pkgs; [
+    osm-python
+    osmium-tool
+    libosmium
+  ];
+  shellHook = ''
+    export PS1="$(echo -e '\uf277') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+  '';
+}
