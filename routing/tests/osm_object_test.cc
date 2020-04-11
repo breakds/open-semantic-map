ee884549991603e0ff7a5b53b9adce36de4e4a7f
Break Yang <breakds@gmail.com>
2020-04-11 13:33:02 -0700

    Update unit test with test data.

--- routing/tests/osm_object_test.cc
+++ routing/tests/osm_object_test.cc
@@ -1,3 +1,4 @@
+#include "osmium/geom/haversine.hpp"
 #include "osmium/handler.hpp"
 #include "osmium/io/pbf_input.hpp"
 #include "osmium/io/reader_with_progress_bar.hpp"
@@ -5,10 +6,17 @@
 #include "osmium/osm.hpp"
 #include "osmium/visitor.hpp"
 
-#include "gtest/gtest.h"
+#include <functional>
+
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 #include "spdlog/spdlog.h"
+
+#include "tests/testdata.h"
+
+namespace open_semap {
+namespace testing {
 
 class ReadWayHandler : public osmium::handler::Handler {
  public:
@@ -20,10 +28,13 @@ class ReadWayHandler : public osmium::handler::Handler {
 };
 
 TEST(OsmObjectTest, ReadWay) {
-  osmium::io::File input_file("/home/breakds/dataset/osm/kirkwood.osm");
+  osmium::io::File input_file(std::string(TEST_DATA_PATH) + "/adobe_wells.osm");
   osmium::io::ReaderWithProgressBar reader(true, input_file,
                                            osmium::osm_entity_bits::way);
   ReadWayHandler handler;
   osmium::apply(reader, handler);
   reader.close();
 }
+
+}  // namespace testing
+}  // namespace open_semap
